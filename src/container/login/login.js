c0ca7e78507a2e0c6c07138da3b57e251420cc92
WhenZ <18622076193@163.com>
2018-05-07 21:22:11 +0800

    登陆注册页面实现

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Logo from '../../component/logo/logo' 
+import {List, InputItem, Button, WingBlank, WhiteSpace} from 'antd-mobile'
 
 class Login extends React.Component{
+    constructor(props){
+        super(props)
+        this.register = this.register.bind(this)
+    }
+    register(){
+        this.props.history.push('/register')
+    }
 
     render(){
         return(
             <div>
                 <Logo/>
                 <h2>登录页</h2>
+                <WingBlank>
+                    <List>
+                        <InputItem>用户名</InputItem>
+                        <InputItem>密码</InputItem>
+                    </List>
+                    <WhiteSpace/>
+                    <Button type='primary'>登陆</Button>
+                    <WhiteSpace/>
+                    <Button type='primary' onClick={this.register}>注册</Button>
+                </WingBlank>
+                
             </div>
         )
     }
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Logo from '../../component/logo/logo' 
+import {Radio, List, InputItem, Button, WingBlank, WhiteSpace} from 'antd-mobile'
+
  
 class Register extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            type:'genius'
+        }
+    }
 
     render(){
+        const RadioItem = Radio.RadioItem
+
         return(
             <div>
                 <Logo/>
                 <h2>注册页</h2>
+                <WingBlank>
+                    <List>
+                        <InputItem>用户名</InputItem>
+                        <InputItem>密码</InputItem>
+                        <InputItem>确认密码</InputItem>
+                    </List>
+                    <RadioItem  checked={this.state.type == 'genius'}>
+                    牛人    
+                    </RadioItem>
+                    <RadioItem  checked={this.state.type == 'boss'}>
+                    BOSS    
+                    </RadioItem>
+                    <WhiteSpace/>
+                    <Button type='primary'>确定</Button>
+                    
+                </WingBlank>
             </div>
         )
     }

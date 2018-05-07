e74423db7cb76d8ed7ece2d9342109ddfc03b271
WhenZ <18622076193@163.com>
2018-05-07 14:22:40 +0800

    first commit

---
+++ src/container/login/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+ 
+class Login extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h2>登录页</h2>
+            </div>
+        )
+    }
+}
+
+export default Login
---
+++ src/container/register/register.js
@@ -0,0 +1,14 @@
+import React from 'react'
+ 
+class Register extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h2>注册页</h2>
+            </div>
+        )
+    }
+}
+
+export default Register

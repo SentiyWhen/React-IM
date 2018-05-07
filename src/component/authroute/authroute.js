40066d849503b17bd3d94b17a0ef781d6dfb3841
张文文 <18622076193@163.com>
2018-05-07 22:57:01 +0800

    用户信息校验 跳转登陆

--- src/component/authroute/authroute.js
+++ src/component/authroute/authroute.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import axios from 'axios'
+import {withRouter} from 'react-router-dom'
 
+@withRouter
 class AuthRoute extends React.Component{
     componentDidMount(){
+        const publicList = ['/login','/register']
+        const pathname = this.props.location.pathname
+        if (publicList.indexOf(pathname)>-1) {
+            return null
+        }
         axios.get('/user/info').
          then(res=>{
              if (res.status==200) {
-                 console.log(res.data)
+                 if (res.data.code == 0) {
+                     //
+                 } else {
+                    this.props.history.push('/login') 
+                 }
              }
          })
     }

337da59c052e9e1bb5ea502791f3d7336296b225
张文文 <18622076193@163.com>
2018-05-07 23:16:28 +0800

    注册交互

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -7,8 +7,21 @@ class Register extends React.Component{
     constructor(props){
         super(props)
         this.state = {
+            user:'',
+            pwd:'',
+            repeatpwd:'',
             type:'genius'
         }
+        this.handleRegister = this.handleRegister.bind(this)
+    }
+    handleChange(key,val){
+        console.log(`${key}--${val}`)
+        this.setState({
+            [key]:val
+        })
+    }
+    handleRegister(){
+        console.log(this.state)
     }
 
     render(){
@@ -19,18 +32,18 @@ class Register extends React.Component{
                 <h2>注册页</h2>
                 <WingBlank>
                     <List>
-                        <InputItem>用户名</InputItem>
-                        <InputItem>密码</InputItem>
-                        <InputItem>确认密码</InputItem>
+                        <InputItem onChange={v=>this.handleChange('user',v)}>用户名</InputItem>
+                        <InputItem onChange={v=>this.handleChange('pwd',v)}>密码</InputItem>
+                        <InputItem onChange={v=>this.handleChange('repeatpwd',v)}>确认密码</InputItem>
                     </List>
-                    <RadioItem checked={this.state.type == 'genius'}>
+                    <RadioItem checked={this.state.type == 'genius'} onChange={()=>this.handleChange('type','genius')}>
                     牛人    
                     </RadioItem>
-                    <RadioItem checked={this.state.type == 'boss'}>
+                    <RadioItem checked={this.state.type == 'boss'} onChange={()=>this.handleChange('type','boss')}>
                     BOSS    
                     </RadioItem>
                     <WhiteSpace/>
-                    <Button type='primary'>确定</Button>
+                    <Button type='primary' onClick={this.handleRegister}>确定</Button>
                     
                 </WingBlank>
             </div>

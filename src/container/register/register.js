c311f8e4b2f5839100ac4fcaa500a9163bb120e1
张文文 <18622076193@163.com>
2018-05-08 09:53:12 +0800

    改动一下register

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,54 +1,45 @@
 import React from 'react'
-import Logo from '../../component/logo/logo' 
-import {Radio, List, InputItem, Button, WingBlank, WhiteSpace} from 'antd-mobile'
+import Logo from '../../component/logo/logo'
+import {List, InputItem,Radio, WingBlank, WhiteSpace, Button} from 'antd-mobile'
 
- 
 class Register extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            user:'',
-            pwd:'',
-            repeatpwd:'',
-            type:'genius'
-        }
-        this.handleRegister = this.handleRegister.bind(this)
-    }
-    handleChange(key,val){
-        console.log(`${key}--${val}`)
-        this.setState({
-            [key]:val
-        })
-    }
-    handleRegister(){
-        console.log(this.state)
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			user:'',
+			pwd:'',
+			repeatpwd:'',
+			type:'genius' // 或者boss
+		}
 
-    render(){
-        const RadioItem = Radio.RadioItem
-        return(
-            <div>
-                <Logo/>
-                <h2>注册页</h2>
-                <WingBlank>
-                    <List>
-                        <InputItem onChange={v=>this.handleChange('user',v)}>用户名</InputItem>
-                        <InputItem onChange={v=>this.handleChange('pwd',v)}>密码</InputItem>
-                        <InputItem onChange={v=>this.handleChange('repeatpwd',v)}>确认密码</InputItem>
-                    </List>
-                    <RadioItem checked={this.state.type == 'genius'} onChange={()=>this.handleChange('type','genius')}>
-                    牛人    
-                    </RadioItem>
-                    <RadioItem checked={this.state.type == 'boss'} onChange={()=>this.handleChange('type','boss')}>
-                    BOSS    
-                    </RadioItem>
-                    <WhiteSpace/>
-                    <Button type='primary' onClick={this.handleRegister}>确定</Button>
-                    
-                </WingBlank>
-            </div>
-        )
-    }
+		this.handleRegister = this.handleRegister.bind(this)
+	}
+	handleChange(key,val){
+		this.setState({
+			[key]:val
+		})
+	}
+	handleRegister(){
+		console.log(this.state)
+	}
+	render(){
+		const RadioItem = Radio.RadioItem
+		return (
+			<div>
+				<Logo></Logo>
+				<List>
+					<InputItem onChange={v=>this.handleChange('user',v)}>用户名</InputItem>
+					<InputItem type='password' onChange={v=>this.handleChange('pwd',v)}>密码</InputItem>
+					<InputItem type='password' onChange={v=>this.handleChange('repeatpwd',v)}>确认密码</InputItem>
+					<RadioItem checked={this.state.type=='genius'} onChange={()=>this.handleChange('type','genius')}>牛人</RadioItem>
+					<RadioItem checked={this.state.type=='boss'} onChange={()=>this.handleChange('type','boss')}>BOSS</RadioItem>
+					<WhiteSpace />
+					<Button type='primary' onClick={this.handleRegister}>注册 </Button>
+				</List>
+			</div>
+
+		)
+	}
 }
 
 export default Register

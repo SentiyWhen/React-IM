ca0461b12b5ff3e3c70f1aa16b13ec2cf046bc22
张文文 <18622076193@163.com>
2018-05-10 16:50:46 +0800

    添加dashboard文件 添加图片

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import Register from './container/register/register'
 import AuthRoute from './component/authroute/authroute'
 import BossInfo from './container/bossinfo/bossinfo'
 import GeniusInfo from './container/geniusinfo/geniusinfo'
+import Dashboard from './component/dashboard/dashboard'
+
 
 import reducers from './reducer'
 import './config'
@@ -29,10 +31,13 @@ ReactDom.render(
 		<BrowserRouter>
 			<div>
 				<AuthRoute></AuthRoute>
-				<Route path='/bossinfo' component={BossInfo}></Route>				
-				<Route path='/geniusinfo' component={GeniusInfo}></Route>				
-				<Route path='/login' component= {Login}></Route>
-				<Route path='/register' component= {Register}></Route>
+				<Switch>
+					<Route path='/bossinfo' component={BossInfo}></Route>				
+					<Route path='/geniusinfo' component={GeniusInfo}></Route>				
+					<Route path='/login' component= {Login}></Route>
+					<Route path='/register' component= {Register}></Route>
+					<Route component={Dashboard}></Route>
+				</Switch>
 			</div>
 		</BrowserRouter>
 	</Provider>),
